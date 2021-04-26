34e4d54f4cc739ffccfb70a67932833251e6936e
cwelshofer <cwelshofer@gmail.com>
2021-04-26 16:46:17 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,53 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Spelling Test</h1>
+    <div v-if="!testFinished">
+      <p>Word {{ activeIndex + 1 }} of {{ questions.length }}</p>
+      <speech :word="questions[activeIndex].word" />
+      <form @submit.prevent="handleSubmit">
+        <input
+          type="text"
+          v-model="userInput"
+          spellcheck="false"
+          placeholder="Spell the word"
+        />
+        <button type="submit">SUBMIT</button>
+      </form>
+    </div>
+    <div v-else>
+      <score :data="questions" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import data from "./data";
+import Speech from "./components/Speech";
+import Score from "./components/Score";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  components: { Speech, Score },
+  data() {
+    return {
+      questions: data,
+      activeIndex: 0,
+      userInput: ""
+    };
+  },
+  methods: {
+    handleSubmit() {
+      this.questions[this.activeIndex].userInput = this.userInput;
+      this.activeIndex += 1;
+      this.userInput = "";
+    }
+  },
+  computed: {
+    testFinished() {
+      return this.questions.every(q => q.userInput);
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Speech.vue
@@ -0,0 +1,21 @@
+<template>
+  <button @click="sayWord" class="clickable">Say Word</button>
+</template>
+
+<script>
+export default {
+props: ["word"],
+
+methods: {
+    props: ["word"],
+    sayWord() {
+      const utterance = new SpeechSynthesisUtterance(this.word);
+      speechSynthesis.speak(utterance);
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
